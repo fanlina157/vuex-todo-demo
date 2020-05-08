246f35932da46ff4526d2919fdbb8e05a02f8573
范丽娜 <fanln@mail.taiji.com.cn>
2020-05-08 09:48:25 +0800

    todo 练习

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 不使用v-model 是 不直接操作inputValue  -->
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="changeValue" />
+    <a-button type="primary" @click="addTodoItem">添加事项</a-button>
+    <!-- 这里换成infoList  getters 包装过的 -->
+    <a-list bordered :dataSource="infoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <!-- 选中与未选中 -->
+        <a-checkbox :checked="item.done" @change="changeCheckbox(item.id)">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="deleteTodoItem(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{count}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewKey === 1 ? 'primary' : 'default'" @click="searchList(1)">全部</a-button>
+          <a-button :type="viewKey === 2 ? 'primary' : 'default'"  @click="searchList(2)">未完成</a-button>
+          <a-button :type="viewKey === 3 ? 'primary' : 'default'"  @click="searchList(3)">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="deleteAll">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      // list: []  // 不使用私有的，使用state 中的
+    }
+  },
+  computed: {
+    ...mapState(['inputValue', 'list', 'viewKey']),
+    ...mapGetters(['count', 'infoList'])
+  },
+  methods: {
+    changeValue (e) {
+      // console.log(e.target.value)
+      this.$store.commit('changeInputValue', e.target.value)
+    },
+    addTodoItem () {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning('不能为空')
+      }
+      this.$store.commit('addItem')
+    },
+    deleteTodoItem (ID) {
+      // alert(ID)
+      this.$store.commit('deleteItem', ID)
+    },
+    changeCheckbox (ID) {
+      // alert(ID)
+      this.$store.commit('changeCheckbox', ID)
+    },
+    deleteAll () {
+      this.$store.commit('cleanDone')
+    },
+    searchList (key) {
+      this.$store.commit('changeKey', key)
+    }
+  },
+  created () {
+    this.$store.dispatch('getList')
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,92 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 所有任务列表
+    list: [],
+    // 文本框输入
+    inputValue: '',
+    nextId: 6,
+    viewKey: 1
   },
   mutations: {
+    // 异步 todoList
+    initList (state, list) {
+      state.list = list
+    },
+    // 同步 input 框输入双向绑定
+    changeInputValue (state, newVal) {
+      state.inputValue = newVal
+    },
+    // 同步 添加todoItem  修改list 并inputValue 清空
+    addItem (state) {
+      const params = {
+        // id: state.list.length,   删除的时候id 会重复
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(params)
+      state.nextId++
+      state.inputValue = ''
+    },
+    // 同步 删除todoITem
+    deleteItem (state, ID) {
+      const index = state.list.findIndex(item => item.id === ID)
+      if (index !== -1) {
+        state.list.splice(index, 1)
+      }
+    },
+    // 修改复选框选中状态
+    changeCheckbox (state, ID) {
+      // 也可以通过 e.target.checked 属性
+      const index = state.list.findIndex(item => item.id === ID)
+      if (index !== -1) {
+        // 哎呀呀---------- 通过index 找到done
+        state.list[index].done = !state.list[index].done
+      }
+    },
+    // 清除已完成
+    cleanDone (state) {
+      state.list = state.list.filter(item => item.done === false)
+    },
+    // 清除已完成
+    changeKey (state, key) {
+      state.viewKey = key
+    }
   },
   actions: {
+    getList: (context) => {
+      axios.get('/list.json').then(({ data }) => {
+        // console.log(data)
+        context.commit('initList', data)
+      })
+    }
+  },
+  getters: {
+    // 统计未完成条数
+
+    // countList: (state) => {
+    //   return state.list.filter(item => item.done === false)
+    // }
+    count: (state) => {
+      return state.list.filter(item => item.done === false).length
+    },
+
+    // 根据不同按钮来显示不同的list
+    // 写到getters 里是因为 筛选的时候 不会更改 state.list  写到 Mutations  会被更改
+    infoList: (state) => {
+      if (state.viewKey === 2) {
+        return state.list.filter(item => item.done === false)
+      } else if (state.viewKey === 3) {
+        return state.list.filter(item => item.done === true)
+      } else {
+        return state.list
+      }
+    }
   },
   modules: {
   }
